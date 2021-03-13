c323bb4820815f80de4825f6d331a1a9718c3ce1
ardhikarn <ardhikarn@gmail.com>
2021-03-13 09:47:06 +0700

    design notes

---
+++ resources/src/components/Form.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="formNotes">
+    <div class="menu">
+      <button type="button" class="bg-danger btn btn-delete">Hapus</button>
+      <button type="submit" class="bg-success btn btn-submit">Simpan</button>
+    </div>
+
+    <div class="content">
+      <input type="text" class="text" placeholder="Title" />
+      <textarea
+        type="text"
+        class="text text-area"
+        placeholder="Tulis Notemu"
+      ></textarea>
+    </div>
+  </div>
+</template>
+
+<style>
+.menu {
+  background: #f7f7f7;
+  padding: 10px 25px;
+  margin-bottom: 25px;
+  text-align: right;
+  border-bottom: 1px solid #e8e6e6;
+}
+.btn-delete {
+  margin-right: 10px;
+}
+.content {
+  padding: 0px 25px;
+}
+.text {
+  display: block;
+  width: 100%;
+  padding: 0px;
+  font-size: 20px;
+  font-weight: bold;
+  color: #2c3e50;
+  border: none;
+  margin-bottom: 10px;
+  box-sizing: border-box;
+  outline: none;
+}
+.textarea {
+  min-height: 350px;
+  font-size: 15px;
+  font-weight: lighter;
+  line-height: 30px;
+}
+.loader {
+  vertical-align: middle;
+}
+</style>
---
+++ resources/src/components/List.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="listNotes">
+    <ul>
+      <li v-for="(note, i) in notes" :key="i">
+        <button class="btn-note">
+          <label>{{ note.title }}</label>
+          <span>{{ note.description }}</span>
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'listNotes',
+  data() {
+    return {
+      notes: [
+        { title: 'rythz', description: 'ini isi note' },
+        { title: 'rythz', description: 'ini isi note' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0px;
+}
+.btn-note {
+  text-align: left;
+  border: none;
+  border-bottom: 1px solid gainsboro;
+  padding: 0px 15px;
+  cursor: pointer;
+  outline: none;
+  background: #f7f7f7;
+  height: 150px;
+  width: 100%;
+}
+.btn-note:hover {
+  background: #eaeaea;
+}
+.btn-note label {
+  display: block;
+  color: #444444;
+  font-size: 1.5em;
+  margin-bottom: 15px;
+}
+.btn-note span {
+  color: #545454;
+}
+</style>
